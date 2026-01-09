5e8283a65a8daafbe70320c22d87561134b29c83
vule vule <127794519+vukasinzi@users.noreply.github.com>
2026-01-09 20:39:57 +0000

    pomoc 2.0

--- build/CMakeFiles/main.dir/src/main.c.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/main.dir/src/main.c.o: /workspaces/asd/src/main.c \
- /usr/include/stdc-predef.h /workspaces/asd/header/kki.h \
- /workspaces/asd/header/defs.h /workspaces/asd/header/misc.h
